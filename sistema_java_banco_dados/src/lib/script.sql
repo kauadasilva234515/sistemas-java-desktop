9a4b75c5cc3c8af06dc47557b7c6555680f533c8
kaua.silveira1 <kauadasilvasilveira14@gmail.com>
2022-09-20 15:57:34 -0300

    ok

--- sistema_java_banco_dados/src/lib/script.sql
+++ sistema_java_banco_dados/src/lib/script.sql
@@ -3,3 +3,14 @@
  * Created: 14 de set. de 2022
  */
 
+-- criando tabelas do banco de dados...
+
+create table public.tbpessoa( 
+    pescodigo serial not null,
+    pesnome varchar(200) not null,
+    pesendereco varchar(200) not null,
+    pescpf varchar(20) not null,
+    CONSTRAINT pk_tbpessoa 
+    PRIMARY KEY (pescodigo)
+);
+
